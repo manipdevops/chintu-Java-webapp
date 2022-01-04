1ead76b9c73bcab299422bdcc180c70e5553e68a
devopsclouds <83501971+devopsclouds@users.noreply.github.com>
2022-01-04 14:43:26 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,13 @@
+# docker-maven-
+
+#!bin/bash
+
+echo "started to creating docker image using maveen build"
+
+docker build -t docker-maven:1.0 .
+
+echo "craeting the container for running application"
+
+docker run -d --name customerapplication -p 9999:8080 docker-maven:1.0 
+
+
